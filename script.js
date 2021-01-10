64c4359df118a6ac33be2bb3d7a33018b869ef90
Jack Ceballos <jackrceballos@gmail.com>
2021-01-09 20:35:04 -0500

    Starting over to get a good grade on the assignment.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
     // Variables
-    var currentDay = $("#currentDay");
     var timeBlock = $(".time-block");
     var am9 = $("#9");
     var am10 = $("#10");
@@ -14,58 +13,11 @@ $(document).ready(function() {
     var pm5 = $("#17");
     var saveBtn = $(".saveBtn");
     
-    // This function will change the color if the time is different
-    function colorChange() {
-        
-        timeBlock.each(function () {
-            var hour = moment().hour();
-            var scheduleTime = $(this).attr("id");
-            
-            if (scheduleTime > hour) {
-                $(this).addClass("future");
-                $(this).removeClass("past");
-                $(this).removeClass("present");
-            }
-            else if (scheduleTime == hour) {
-                $(this).addClass("present");
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-            }
-            else {
-                $(this).addClass("past");
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-            }
-            console.log(this);
-            console.log(scheduleTime);
-        })
-    }
-    
-    // Click to save the text from textarea to storage
-    saveBtn.on("click", function() {
-        var description = $(this).siblings(".description").val();
-        var scheduleTime = $(this).parent().attr("id");
-        
-        localStorage.setItem(scheduleTime, description);
-        
-        console.log(this);
-        console.log(description);
-    })
-    
-    // This is where all of the data is stored
-    $(".description").val(localStorage.getItem(am9));
-    $(".description").val(localStorage.getItem(am10));
-    $(".description").val(localStorage.getItem(am11));
-    $(".description").val(localStorage.getItem(pm12));
-    $(".description").val(localStorage.getItem(pm1));
-    $(".description").val(localStorage.getItem(pm2));
-    $(".description").val(localStorage.getItem(pm3));
-    $(".description").val(localStorage.getItem(pm4));
-    $(".description").val(localStorage.getItem(pm5));
-    
-    // This determines the current time display in the Jumbotron
-    currentDay.text(moment().format("dddd, MMMM Do YYYY"));
+    // Displays date in the jumbotron
+    var dateDisplay = $("#day");
+    var date = moment().format("dddd, MMM Do YYYY, h:mm a");
+    dateDisplay.text(date);
 
-    // Calls the function to run
-    colorChange();
+    
+    
 })
