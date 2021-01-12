2b2b717a9b315cc885815dd085095fbb985d2482
Jack Ceballos <jackrceballos@gmail.com>
2021-01-11 21:19:46 -0500

    Added new loop to the function

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-
-    // Variables
+     // Variables
     var am9 = $("#9");
     var am10 = $("#10");
     var am11 = $("#11");
@@ -12,45 +11,51 @@ $(document).ready(function() {
     var pm5 = $("#17");
     var saveBtn = $(".saveBtn");
     var hour = moment().hour();
+
     
-    // Array to loop through
-    var timeArr = [am9, am10, am11, pm12, pm1, pm2, pm3, pm4, pm5];
-
-    // Displays date in the jumbotron
-    var dateDisplay = $("#day");
-    var date = moment().format("dddd, MMM Do YYYY, h:mm a");
-    dateDisplay.text(date);
-
-    // Function to change the classes of the time blocks
-    function blockChange(array) {
-        
-        // Variable to grab the id number to compare with the hour
-        var currentTime = array.attr("id");
-
-        if (currentTime == hour) {
-            array.addClass("present");
-            array.removeClass("future");
-        }
-        else if (currentTime > hour) {
-            array.addClass("future");
-            array.removeClass("past");
-        }
-        else {
-            array.addClass("past");
-            array.removeClass("present");
-        }
-    };
-
+    // Saves the items to local storage
     saveBtn.on("click", function() {
         var description = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-        localStorage.setItem(time, description);
-        console.log(time);
-    })
+        var stringJSON = JSON.stringify(time);
+
+        localStorage.setItem(stringJSON, description);
+    });
     
-    
-    // For loops to repeat through the function
-    for (let i = 0; i < timeArr.length; i++){
-        blockChange(timeArr[i]);
+    $(".description").val(localStorage.getItem(am9.attr("id")));
+    $(".description").text(localStorage.getItem(pm5.attr("id")));
+
+
+    // Function to change the classes of the time blocks
+    function blockChange() {
+        
+        // Array to loop through
+        var timeArr = [am9, am10, am11, pm12, pm1, pm2, pm3, pm4, pm5];
+        
+        // Loop through array to change colors of the time blocks
+        for(let i = 0; i < timeArr.length; i++) {    
+            // Variable to grab the id number to compare with the hour
+            var currentTime = timeArr[i].attr("id");
+            
+            if (currentTime == hour) {
+                timeArr[i].addClass("present");
+                timeArr[i].removeClass("future");
+            }
+            else if (currentTime > hour) {
+                timeArr[i].addClass("future");
+                timeArr[i].removeClass("past");
+            }
+            else {
+                timeArr[i].addClass("past");
+                timeArr[i].removeClass("present");
+            }
+        }    
     };
-})
+    
+    // Displays date in the jumbotron
+    var dateDisplay = $("#day");
+    var date = moment().format("dddd, MMM Do YYYY");
+    dateDisplay.text(date);
+    
+    blockChange();
+});
