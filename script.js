b18f1b798794cb79e9a744ea36a1935f078cacb6
Jack Ceballos <jackrceballos@gmail.com>
2021-01-11 21:50:35 -0500

    Code is finalized I finally found out how to get the individual descriptions.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-     // Variables
+    // Variables
     var am9 = $("#9");
     var am10 = $("#10");
     var am11 = $("#11");
@@ -11,20 +11,19 @@ $(document).ready(function() {
     var pm5 = $("#17");
     var saveBtn = $(".saveBtn");
     var hour = moment().hour();
-
+    
+    // Displays date in the jumbotron
+    var dateDisplay = $("#day");
+    var date = moment().format("dddd, MMM Do YYYY");
+    dateDisplay.text(date);
     
     // Saves the items to local storage
     saveBtn.on("click", function() {
         var description = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-        var stringJSON = JSON.stringify(time);
 
-        localStorage.setItem(stringJSON, description);
+        localStorage.setItem(time, description);
     });
-    
-    $(".description").val(localStorage.getItem(am9.attr("id")));
-    $(".description").text(localStorage.getItem(pm5.attr("id")));
-
 
     // Function to change the classes of the time blocks
     function blockChange() {
@@ -32,30 +31,30 @@ $(document).ready(function() {
         // Array to loop through
         var timeArr = [am9, am10, am11, pm12, pm1, pm2, pm3, pm4, pm5];
         
-        // Loop through array to change colors of the time blocks
+        // Loop through array to change colors of the time blocks and get stored information
         for(let i = 0; i < timeArr.length; i++) {    
-            // Variable to grab the id number to compare with the hour
-            var currentTime = timeArr[i].attr("id");
-            
-            if (currentTime == hour) {
+            // Variable to grab the id number to compare with the hour and the description of the hour
+            var workTime = timeArr[i].attr("id");
+            var hourDesc = timeArr[i].children(".description");
+
+            if (workTime == hour) {
                 timeArr[i].addClass("present");
                 timeArr[i].removeClass("future");
+                hourDesc.val(localStorage.getItem(workTime));
             }
-            else if (currentTime > hour) {
+            else if (workTime > hour) {
                 timeArr[i].addClass("future");
                 timeArr[i].removeClass("past");
+                hourDesc.val(localStorage.getItem(workTime));
             }
             else {
                 timeArr[i].addClass("past");
                 timeArr[i].removeClass("present");
+                hourDesc.val(localStorage.getItem(workTime));
             }
         }    
     };
-    
-    // Displays date in the jumbotron
-    var dateDisplay = $("#day");
-    var date = moment().format("dddd, MMM Do YYYY");
-    dateDisplay.text(date);
-    
+
+    // Function being called
     blockChange();
 });
